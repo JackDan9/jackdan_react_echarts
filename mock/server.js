62ea79580061307a1327bcaef113ecea60f5aaae
JackDan9 <1835812864@qq.com>
2018-04-01 21:03:55 +0800

    :cn: jackdan : react : echarts

--- mock/server.js
+++ mock/server.js
@@ -6,6 +6,7 @@ app.use(function *(){
     this.set('Access-Control-Allow-Origin','*');
     this.body = {name: 'czl'};
 });
+
 // 开始服务并生成路由
 app.use(router.routes())
    .use(router.allowedMethods());
