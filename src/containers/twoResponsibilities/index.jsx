8345d0b497e2a98c47d22dd09a4a8296c34676a8
jackdan <1835812864@qq.com>
2022-05-30 10:26:55 +0800

    :cn: Update custom_echarts module;

--- src/containers/twoResponsibilities/index.jsx
+++ src/containers/twoResponsibilities/index.jsx
@@ -31,34 +31,29 @@ export default class twoRes extends Component {
     const mainCanvas = document.getElementById("mainCanvas");
     const ctx = mainCanvas.getContext("2d");
 
-    const gridSize = 83;
+    const gridSize = 56;
 
     const canvasWidth = ctx.canvas.width;
     const canvasHeight = ctx.canvas.height;
-
-    // const canvasWidth = _canvasWidth * 0.9;
-    // const canvasHeight = _canvasHeight * 0.9;
-
+    const halfCanvasWidth = canvasWidth / 2;
+    const halfCanvasHeight = canvasHeight / 2;
 
     const xLineTotals = Math.floor(canvasHeight / gridSize); // 计算需要绘画的x轴条数
-    for (let i = 0; i <= xLineTotals; i++) {
+
+    for (let i = 0; i <= xLineTotals + 1; i++) {
       ctx.beginPath(); // 开启路径，设置不同的样式
-      ctx.moveTo(0, gridSize * i); // 起点位置
-      ctx.lineTo(canvasWidth, gridSize * i); // 终点位置
+      ctx.moveTo(0, gridSize * i + 0.5); // 起点位置
+      ctx.lineTo(canvasWidth, gridSize * i + 0.5); // 终点位置
       ctx.strokeStyle = "#000"; // 每个线条的颜色
       ctx.stroke();
     }
 
-    // for(let k = 0; k <= xLineTotals; k++) {
-    //   ctx.beginPath();
-    //   ctx.moveTo(0, gridSize * i);
-    //   ctx.lineTo(canvasWidth, (gridSize * i) / 2);
-    //   ctx.strokeStyle = "#ccc";
-    //   ctx.stroke();
-    // }
-
-
     const yLineTotals = Math.floor(canvasWidth / gridSize);
+
+    console.log("xLineTotals: ", xLineTotals);
+    console.log("canvasHeight: ", canvasHeight);
+    console.log("canvasWidth: ", canvasWidth);
+    console.log("yLineTotals: ", yLineTotals);
     for (let j = 0; j <= yLineTotals; j++) {
       ctx.beginPath();
       ctx.moveTo(gridSize * j, 0);
@@ -66,6 +61,7 @@ export default class twoRes extends Component {
       ctx.strokeStyle = "#000";
       ctx.stroke();
     }
+
     // ctx.textAlign = "center";
     
     console.log("mainCanvas: ", mainCanvas);
@@ -80,7 +76,7 @@ export default class twoRes extends Component {
         </div> */}
         <div className={style.container} style={{ inset: '109px 40px 35px 225px', background: 'rgb(229, 229, 229)' }}>
           {/* {this.props.children} */}
-          <canvas className={style.mainCanvas} style={{ position: 'fixed', margin: 32, width: 1167, background: 'rgb(255, 255, 255)'}} id="mainCanvas" width={2334} height={664}></canvas>
+          <canvas className={style.mainCanvas} style={{ position: 'fixed', margin: 32, background: 'rgb(255, 255, 255)'}} id="mainCanvas" width={2334} height={900}></canvas>
         </div>
       </div>
     )
