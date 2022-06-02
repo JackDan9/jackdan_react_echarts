d3cfb0d58ca7a41398716d65fde76c6aa3e088c8
jackdan <1835812864@qq.com>
2022-06-02 22:52:01 +0800

    :cn: Update custom_echarts module;

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -4,6 +4,7 @@
  * @Date 2022-05-30
  */
 import React, { Component } from "react";
+import Line from "./Line";
 import Map from "./Map";
 import style from "./style.scss";
 
@@ -21,7 +22,7 @@ class Chart extends Component {
       <div className={style.chartContainer}>
         <div className={style.leftContainer}>
           <div className={style.content}>
-            
+            <Line />
           </div>
         </div>
         <div className={style.centerContainer}>
