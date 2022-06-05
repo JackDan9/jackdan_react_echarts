d5668a9ac92e4345bb21c3381e612d04be42caf3
jackdan <1835812864@qq.com>
2022-06-05 08:55:06 +0800

    :cn: Update `custom chart` module;

--- src/components/Chart/Scatter/index.jsx
+++ src/components/Chart/Scatter/index.jsx
@@ -5,6 +5,7 @@
  */
 import React, { Component } from 'react';
 import * as echarts from 'echarts';
+import * as ecStat from 'echarts-stat';
 
 import style from './style.scss';
 
@@ -12,6 +13,117 @@ class Scatter extends Component {
   constructor(props) {
     super(props);
     this.myChart = null;
+  }
+
+  renderBoundary = (params, api) => {
+    var xVal = api.value(0);
+    var yVal = api.value(1);
+    var maxDist = api.value(2);
+    var center = api.coord([xVal, yVal]);
+    var size = api.size([maxDist, maxDist]);
+    return {
+      type: 'ellipse',
+      shape: {
+        cx: isNaN(center[0]) ? 0 : center[0],
+        cy: isNaN(center[1]) ? 0 : center[1],
+        rx: isNaN(size[0]) ? 0 : size[0] + 15,
+        ry: isNaN(size[1]) ? 0 : size[1] + 15
+      },
+      extra: {
+        renderProgress: ++targetRenderProgress,
+        enterFrom: {
+          renderProgress: 0
+        },
+        transition: 'renderProgress'
+      },
+      style: {
+        fill: null,
+        stroke: 'rgba(0,0,0,0.2)',
+        lineDash: [4, 4],
+        lineWidth: 4
+      }
+    };
+  }
+
+  renderItemPoint = (params, api) => {
+    var colorAll = [
+      '#bbb',
+      '#37A2DA',
+      '#e06343',
+      '#37a354',
+      '#b55dba',
+      '#b5bd48',
+      '#8378EA',
+      '#96BFFF'
+    ];
+    var coord = api.coord([api.value(0), api.value(1)]);
+    var clusterIdx = api.value(2);
+    if (clusterIdx == null || isNaN(clusterIdx)) {
+      clusterIdx = 0;
+    }
+    var isNewCluster = clusterIdx === api.value(3);
+    var extra = {
+      transition: []
+    };
+    var contentColor = colorAll[clusterIdx];
+    return {
+      type: 'circle',
+      x: coord[0],
+      y: coord[1],
+      shape: {
+        cx: 0,
+        cy: 0,
+        r: 10
+      },
+      extra: extra,
+      style: {
+        fill: contentColor,
+        stroke: '#333',
+        lineWidth: 1,
+        shadowColor: contentColor,
+        shadowBlur: isNewCluster ? 12 : 0,
+        transition: ['shadowBlur', 'fill']
+      }
+    };
+  }
+
+  makeStepOption = (option, data, centroids) => {
+    var CENTER_DIM_IDX = [3, 4];
+    var params = null;
+    var DIM_CLUSTER_INDEX = 2;
+    var DATA_DIM_IDX = [0, 1];
+    var newCluIdx = centroids ? centroids.length - 1 : -1;
+    var maxDist = 0;
+    for (var i = 0; i < data.length; i++) {
+      var line = data[i];
+      if (line[DIM_CLUSTER_INDEX] === newCluIdx) {
+        var dist0 = Math.pow(line[DATA_DIM_IDX[0]] - line[CENTER_DIM_IDX[0]], 2);
+        var dist1 = Math.pow(line[DATA_DIM_IDX[1]] - line[CENTER_DIM_IDX[1]], 2);
+        maxDist = Math.max(maxDist, dist0 + dist1);
+      }
+    }
+    var boundaryData = centroids
+      ? [[centroids[newCluIdx][0], centroids[newCluIdx][1], Math.sqrt(maxDist)]]
+      : [];
+    option.options.push({
+      series: [
+        {
+          type: 'custom',
+          encode: {
+            tooltip: [0, 1]
+          },
+          renderItem: this.renderItemPoint,
+          data: data
+        },
+        {
+          type: 'custom',
+          renderItem: this.renderBoundary,
+          animationDuration: 3000,
+          silent: true,
+          data: boundaryData
+        }
+      ]
+    });
   }
 
   getOption = () => {
@@ -78,7 +190,7 @@ class Scatter extends Component {
       [0.639276, -3.41284]
     ];
     var DIM_CLUSTER_INDEX = 2;
-    var DATA_DIM_IDX = [0, 1];
+    
     var CENTER_DIM_IDX = [3, 4];
     // See https://github.com/ecomfe/echarts-stat
     var step = ecStat.clustering.hierarchicalKMeans(originalData, {
@@ -88,111 +200,9 @@ class Scatter extends Component {
       outputCentroidDimensions: CENTER_DIM_IDX,
       stepByStep: true
     });
-    var colorAll = [
-      '#bbb',
-      '#37A2DA',
-      '#e06343',
-      '#37a354',
-      '#b55dba',
-      '#b5bd48',
-      '#8378EA',
-      '#96BFFF'
-    ];
+    
     var ANIMATION_DURATION_UPDATE = 1500;
-    function renderItemPoint(params, api) {
-      var coord = api.coord([api.value(0), api.value(1)]);
-      var clusterIdx = api.value(2);
-      if (clusterIdx == null || isNaN(clusterIdx)) {
-        clusterIdx = 0;
-      }
-      var isNewCluster = clusterIdx === api.value(3);
-      var extra = {
-        transition: []
-      };
-      var contentColor = colorAll[clusterIdx];
-      return {
-        type: 'circle',
-        x: coord[0],
-        y: coord[1],
-        shape: {
-          cx: 0,
-          cy: 0,
-          r: 10
-        },
-        extra: extra,
-        style: {
-          fill: contentColor,
-          stroke: '#333',
-          lineWidth: 1,
-          shadowColor: contentColor,
-          shadowBlur: isNewCluster ? 12 : 0,
-          transition: ['shadowBlur', 'fill']
-        }
-      };
-    }
-    function renderBoundary(params, api) {
-      var xVal = api.value(0);
-      var yVal = api.value(1);
-      var maxDist = api.value(2);
-      var center = api.coord([xVal, yVal]);
-      var size = api.size([maxDist, maxDist]);
-      return {
-        type: 'ellipse',
-        shape: {
-          cx: isNaN(center[0]) ? 0 : center[0],
-          cy: isNaN(center[1]) ? 0 : center[1],
-          rx: isNaN(size[0]) ? 0 : size[0] + 15,
-          ry: isNaN(size[1]) ? 0 : size[1] + 15
-        },
-        extra: {
-          renderProgress: ++targetRenderProgress,
-          enterFrom: {
-            renderProgress: 0
-          },
-          transition: 'renderProgress'
-        },
-        style: {
-          fill: null,
-          stroke: 'rgba(0,0,0,0.2)',
-          lineDash: [4, 4],
-          lineWidth: 4
-        }
-      };
-    }
-    function makeStepOption(option, data, centroids) {
-      var newCluIdx = centroids ? centroids.length - 1 : -1;
-      var maxDist = 0;
-      for (var i = 0; i < data.length; i++) {
-        var line = data[i];
-        if (line[DIM_CLUSTER_INDEX] === newCluIdx) {
-          var dist0 = Math.pow(line[DATA_DIM_IDX[0]] - line[CENTER_DIM_IDX[0]], 2);
-          var dist1 = Math.pow(line[DATA_DIM_IDX[1]] - line[CENTER_DIM_IDX[1]], 2);
-          maxDist = Math.max(maxDist, dist0 + dist1);
-        }
-      }
-      var boundaryData = centroids
-        ? [[centroids[newCluIdx][0], centroids[newCluIdx][1], Math.sqrt(maxDist)]]
-        : [];
-      option.options.push({
-        series: [
-          {
-            type: 'custom',
-            encode: {
-              tooltip: [0, 1]
-            },
-            renderItem: renderItemPoint,
-            data: data
-          },
-          {
-            type: 'custom',
-            renderItem: renderBoundary,
-            animationDuration: 3000,
-            silent: true,
-            data: boundaryData
-          }
-        ]
-      });
-    }
+    
     var targetRenderProgress = 0;
     let option = {
       timeline: {
@@ -233,10 +243,10 @@ class Scatter extends Component {
       },
       options: []
     };
-    makeStepOption(option, originalData);
+    this.makeStepOption(option, originalData);
     option.timeline.data.push('0');
     for (var i = 1, stepResult; !(stepResult = step.next()).isEnd; i++) {
-      makeStepOption(
+      this.makeStepOption(
         option,
         echarts.util.clone(stepResult.data),
         echarts.util.clone(stepResult.centroids)
