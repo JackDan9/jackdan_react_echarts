dbadeea35261fa11ce67a7b79c6f3ce8a9479a5e
jackdan <1835812864@qq.com>
2022-06-25 17:02:02 +0800

    :cn: Add `mock` module;

---
+++ mock/index.js
@@ -0,0 +1,21 @@
+import Mock from 'mockjs';
+import base from './modules/base';
+// const Mock = require('mockjs');
+
+// 修复在使用 MockJS 情况下，设置 withCredentials = true, 且未被拦截的跨域请求丢失 Cookies 的问题
+// https://github.com/nuysoft/Mock/issues/300
+Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send;
+Mock.XHR.prototype.send = function() {
+  if (this.custom.xhr) {
+    this.custom.xhr.withCredentials = this.withCredentials || false;
+  }
+  this.proxy_send(...arguments);
+}
+
+// 模拟真实数据的延迟
+Mock.setup({
+  timeout: '500-800'
+})
+
+Mock.mock(/\/base/, 'get', base.getBase);
+
---
+++ mock/modules/base.js
@@ -0,0 +1,16 @@
+
+const base = [
+  {
+    
+  }
+]
+
+export default {
+  getBase() {
+    return {
+      code: 1,
+      data: base,
+      msg: "A base data from mock data"
+    }
+  }
+}
